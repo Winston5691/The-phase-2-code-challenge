046d7de9b24b7d1829bd5544b21c580b2a5432f0
Winston Langat <winston.langat@student.moringaschool.com>
2022-09-26 11:38:27 +0300

    project

---
+++ src/components/TransactionsList.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Transaction from "./Transaction";
+function TransactionsList({transactions , search}) {
+  const filteredTransactions = transactions.filter ((transaction) =>{
+return transaction.description.includes(search);
+  });
+  console.log(search)
+  const transactionComponents = filteredTransactions.map(transaction =>{
+    return <Transaction key={transaction.id} transaction ={transaction } />;
+  });
+  return (
+    <table className="ui celled striped padded table">
+      <tbody>
+        <tr>
+          <th>
+            <h3 className="ui center aligned header">Date</h3>
+          </th>
+          <th>
+            <h3 className="ui center aligned header">Description</h3>
+          </th>
+          <th>
+            <h3 className="ui center aligned header">Category</h3>
+          </th>
+          <th>
+            <h3 className="ui center aligned header">Amount</h3>
+          </th>
+        </tr>
+        {transactionComponents}
+      </tbody>
+    </table>
+  );
+}
+export default TransactionsList;
